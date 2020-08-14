88d7b66e0e0e4b7a4c996d4f941bd806f363d6e2
Balaji Kummari <balajievlo@gmail.com>
2020-08-14 10:54:33 +0530

    changed edit URL's

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ module.exports = {
         },
         {to: 'blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/balajikummari/docusaurusGenisis',
           label: 'GitHub',
           position: 'right',
         },
@@ -71,12 +71,12 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/balajikummari/docusaurusGenisis',
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} JObberCore , Inc. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -89,13 +89,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/balajikummari/docusaurusGenisis/tree/documentation/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/balajikummari/docusaurusGenisis/tree/documentation/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
